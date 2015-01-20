b850a79784c33180b8d8fce036063f297e6581ec
argunner <gunnerar7@gmail.com>
2015-01-21 00:18:18 +0530

    Source File

---
+++ index.js
@@ -0,0 +1,102 @@
+/*|-------------blinkTitle.js----------------------------------------------------|
+  |-------(Pure Javascript TitleBar Alert Script)----------------------------------|
+  |-----------Author : argunner (gunnerar7@gmail.com)(http://github.com/argunner)|
+  */
+
+
+//To Do's
+// 1) Add Timeout Per Notification
+// 2) Simplify To JSON Arguments   
+
+
+
+
+
+var hold="";
+
+
+
+        function blinkTitle(msg1,msg2,delay,isFocus = false,timeout = false){
+          
+
+          document.title = msg1;
+
+          if(isFocus == false){
+
+            hold = window.setInterval(function() {
+
+              
+
+                if( document.title == msg1){
+
+                  document.title = msg2;
+              
+                }else{
+
+                  document.title = msg1;
+                }
+              
+              }, delay);
+            }
+
+            if(isFocus == true){
+
+
+              var onPage = false;
+              var testflag = true;
+
+              var initialTitle = document.title;
+
+              window.onfocus = function () { 
+                onPage = true; 
+                
+            }; 
+
+            window.onblur = function () { 
+                onPage = false;
+                testflag = false;
+            }; 
+
+            
+
+
+
+              hold = window.setInterval(function() {
+
+
+
+                if( onPage == false){
+              
+
+                  if( document.title == msg1){
+
+                    document.title = msg2;
+              
+                  }else{
+
+                    document.title = msg1;
+                  }
+                }
+
+                            
+                
+              }, delay);
+              
+            }
+
+
+
+
+
+
+          }
+
+          function blinkTitleStop(timeout = false){
+
+
+
+            clearInterval(hold);
+
+        }
+
+        
